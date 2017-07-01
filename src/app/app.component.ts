40241671c245079d36e8ba6225af7bd4f38f000d
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2017-07-01 20:39:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.getHeroesSlowly();
+  }
+  constructor(private heroService: HeroService) { }
+
+  title = 'Ejemplo pendorcho';
+  heroes: Hero[];
+  selectedHero: Hero;
+  getHeroesSlowly(): void {
+    this.heroService.getHeroes()
+      .then(heroes => {
+        return this.heroes = heroes;
+      });
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
