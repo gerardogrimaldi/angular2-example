475a04d9dc2bece05edb7df9ce274ee7ded6c309
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2017-07-26 00:32:41 -0300

    ADMIN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,61 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+import { routing } from './app.routing';
 
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroService }          from './hero.service';
+// App is our top level component
+import { App } from './app.component';
+import { AppState, InternalStateType } from './app.service';
+import { GlobalState } from './global.state';
+import { NgaModule } from './theme/nga.module';
+import { PagesModule } from './pages/pages.module';
 
-import { AppRoutingModule }     from './app-routing.module';
 
+// Application wide providers
+const APP_PROVIDERS = [
+  AppState,
+  GlobalState
+];
+
+export type StoreType = {
+  state: InternalStateType,
+  restoreInputValues: () => void,
+  disposeOldHosts: () => void
+};
+
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
+  bootstrap: [App],
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent
+    App
   ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+  imports: [ // import Angular's modules
+    BrowserModule,
+    HttpModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgaModule.forRoot(),
+    NgbModule.forRoot(),
+    PagesModule,
+    routing
+  ],
+  providers: [ // expose our Services and Providers into Angular's dependency injection
+    APP_PROVIDERS
+  ]
 })
-export class AppModule { }
+
+export class AppModule {
+
+  constructor(public appState: AppState) {
+  }
+}
